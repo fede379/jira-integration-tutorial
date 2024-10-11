78e33da349a0ece2a89ae4186c774630959a6099
Federico Loebarth <federico.loebarth@devlights.com>
2024-10-10 21:47:59 -0300

    remove secrets

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,4 @@
-export const ATLASSIAN_CLIENT_ID = "vILo9cuIgJwJADeFVGaioo4THjAfdNA7";
-export const ATLASSIAN_SECRET =
-  "ATOAO3uX9LWp9mSD3que3yrGJXZ-4XZUd5sDz4axBRza5DQmUHC_yc26TITkZ7u3AG8E40699A96";
+export const ATLASSIAN_CLIENT_ID = import.meta.env.VITE_ATLASSIAN_CLIENT_ID;
+export const ATLASSIAN_SECRET = import.meta.env.VITE_ATLASSIAN_SECRET;
 export const ATLASSIAN_AUTH_URL = "https://auth.atlassian.com/";
 export const ATLASSIAN_API_URL = "https://api.atlassian.com";
